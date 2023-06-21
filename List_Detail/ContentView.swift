7e5fa18c3c012641b6d5cace8f4c6bf8a6d7b03d
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-06-21 22:31:13 +0200

    Initial Commit

---
+++ List_Detail/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  List_Detail
+//
+//  Created by Pavankumar Arepu on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
